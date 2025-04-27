cae9302807186ddd085cee6509237f22a12f3303
Chris Yoxall <chris.yoxall@gmail.com>
2025-04-27 18:03:13 +1200

    Another variation

--- cd-files/jenkins-custom-entrypoint.sh
+++ cd-files/jenkins-custom-entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+# Fix Docker socket permissions
 if [ -e /var/run/docker.sock ]; then
   chmod 666 /var/run/docker.sock
 fi
+
+# Ensure jenkins user has permissions to the workspace directory
+chown -R jenkins:jenkins /var/jenkins_home/workspace
+
+# Continue with normal Jenkins startup
 exec /usr/local/bin/jenkins.sh
